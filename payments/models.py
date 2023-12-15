aa16e8f2bc3aafc371ad7f4d203f6e2d92a5a64f
AttackPatrick <patrickgmlynch@gmail.com>
2023-12-16 01:25:59 +0530

    added payments model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from payments.models import *
+from apartments.models import *
 
-# Create your models here.
+
+
+class Payment(models.Model):
+    booking                     =     models.OneToOneField(Booking,on_delete=models.CASCADE)
+    apartment                   =     models.ForeignKey(Apartment,on_delete=models.CASCADE)
+    payment_recieved            =     models.IntegerField()
+    payment_recieved_date_time  =     models.DateTimeField()
+    created_at                  =     models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self(self.id)
