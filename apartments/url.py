3b4bc5b690d6ea3daaab4353233968cbb1ef9370
AttackPatrick <patrickgmlynch@gmail.com>
2023-12-16 01:01:53 +0530

    Apartments list view and templates

--- apartments/url.py
+++ apartments/url.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', apartments_list, name='apartments_list'),
-    
+    path('', apartments_list, name='apartments_list'),   
 ]
