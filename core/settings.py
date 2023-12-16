2b8e2f5be2111b57180c4588c6efb706e98a030d
AttackPatrick <patrickgmlynch@gmail.com>
2023-12-16 13:34:35 +0530

    template dir settings

--- core/settings.py
+++ core/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -69,15 +70,16 @@ ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [TEMPLATE_DIR],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                'site_config.views.global_context',
             ],
         },
     },
--- static_pages/views.py
+++ static_pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from apartments.models import *
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    apartments = Apartment.objects.filter()
+    context['apartments'] = apartments
+    # return render(request,'index.html',context)
+    return render('apartments_list')
