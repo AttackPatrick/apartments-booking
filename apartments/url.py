6e22599446fa7b3a1b61f74cae6c3d7b3cc1c42c
AttackPatrick <patrickgmlynch@gmail.com>
2023-12-19 21:09:56 +0530

    fixed apartment urls issue

--- apartments/url.py
+++ apartments/url.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', apartments_list, name='apartments_list'),   
+    path('', apartments_list, name='apartments_list'),
+    path('apartments_detail/<id>/', apartments_detail, name='apartments_detail'),
+    path('apartments_booking_succesful/<id>/',
+         apartments_booking_succesful, name='apartments_booking_succesful'),
 ]
